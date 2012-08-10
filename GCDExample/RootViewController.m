ca817aec24fff559e153134d6553c28d11923ae5
Jeff Kelley <SlaunchaMan@gmail.com>
2012-08-10 10:13:49 -0400

    Cache access

--- GCDExample/RootViewController.m
+++ GCDExample/RootViewController.m
@@ -14,9 +14,69 @@
 static const void *kRowKey = &kRowKey;
 
 
+@interface RootViewController() {
+	NSMutableDictionary *cache;
+	dispatch_queue_t cacheQueue;
+}
+
+- (NSMutableDictionary *)cache;
+- (dispatch_queue_t)cacheQueue;
+- (id)objectInCacheForKey:(id)key;
+- (void)setObject:(id)object inCacheForKey:(id)key;
+
+@end
+
+
 @implementation RootViewController {
     NSString	*imageFolder;
     NSArray 	*imagePathArray;
+}
+
+#pragma mark - Cache Access
+
+- (NSMutableDictionary *)cache
+{
+	if (cache == nil) {
+		cache = [[NSMutableDictionary alloc] init];
+	}
+	
+	return cache;
+}
+
+- (dispatch_queue_t)cacheQueue
+{
+	if (cacheQueue == NULL) {
+		cacheQueue = dispatch_queue_create("com.slaunchaman.cachequeue", DISPATCH_QUEUE_CONCURRENT);
+	}
+	
+	return cacheQueue;
+}
+
+- (id)objectInCacheForKey:(id)key
+{
+	__block id object = nil;
+	
+	dispatch_sync([self cacheQueue], ^{
+		object = [[self cache] objectForKey:key];
+	});
+	
+	return object;
+}
+
+- (void)setObject:(id)object inCacheForKey:(id)key
+{
+	dispatch_barrier_async([self cacheQueue], ^{
+		[[self cache] setObject:object forKey:key];
+	});
+}
+
+#pragma mark - Object Lifecycle
+
+- (void)dealloc
+{
+	if (cacheQueue != NULL) {
+		dispatch_release(cacheQueue);
+	}
 }
 
 #pragma mark - View Controller Lifecycle
@@ -29,6 +89,15 @@ static const void *kRowKey = &kRowKey;
     imageFolder = [[resourcePath stringByAppendingPathComponent:@"Nature"] copy];
     imagePathArray = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:imageFolder
                                                                       error:NULL];
+}
+
+- (void)didReceiveMemoryWarning
+{
+	[super didReceiveMemoryWarning];
+	
+	dispatch_barrier_async([self cacheQueue], ^{
+		[[self cache] removeAllObjects];
+	});
 }
 
 #pragma mark - UITableViewDataSource Protocol Methods
@@ -67,22 +136,31 @@ static const void *kRowKey = &kRowKey;
 	NSNumber *rowNumber = @([indexPath row]);
 	
 	objc_setAssociatedObject(cell, kRowKey, rowNumber, OBJC_ASSOCIATION_RETAIN);
-
-	dispatch_queue_t backgroundQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0);
-
-	dispatch_async(backgroundQueue, ^{
-		UIImage *image = [[UIImage alloc] initWithContentsOfFile:imagePath];
+	
+	// Do we have a cached image?
+	UIImage *cachedImage = [self objectInCacheForKey:rowNumber];
+	
+	if (cachedImage != nil) {
+		[[cell imageView] setImage:cachedImage];
+	}
+	else {
+		dispatch_queue_t backgroundQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0);
 		
-		dispatch_async(dispatch_get_main_queue(), ^{
-			NSNumber *cellRow = objc_getAssociatedObject(cell, kRowKey);
+		dispatch_async(backgroundQueue, ^{
+			UIImage *image = [[UIImage alloc] initWithContentsOfFile:imagePath];
 			
-			if ([cellRow isEqualToNumber:rowNumber]) {
-				[[cell imageView] setImage:image];
-				[cell setNeedsLayout];
-			}
+			dispatch_async(dispatch_get_main_queue(), ^{
+				NSNumber *cellRow = objc_getAssociatedObject(cell, kRowKey);
+				
+				if ([cellRow isEqualToNumber:rowNumber]) {
+					[[cell imageView] setImage:image];
+					[cell setNeedsLayout];
+				}
+			});
+			
+			[self setObject:image inCacheForKey:rowNumber];
 		});
-	});
-
+	}
 	    
 	return cell;
 }
